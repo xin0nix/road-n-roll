e8a6b01c4b3cac02a3d8b69d26a9784fb5b343b3
xin0nix <xin0nix@yahoo.com>
2025-02-15 18:50:29 +0300

    Remove custom child vector container

--- core/lib/router/router.hpp
+++ core/lib/router/router.hpp
@@ -122,82 +122,7 @@ protected:
                                 core::string_view *&names) const noexcept;
 };
 
-// a small vector for child nodes...
-// we shouldn't expect many children per node, and
-// we don't want to allocate for that. But we also
-// cannot cap the max number of child nodes because
-// especially the root nodes can potentially an
-// exponentially higher number of child nodes.
-class child_idx_vector {
-  static constexpr std::size_t N = 5;
-  std::size_t static_child_idx_[N]{};
-  std::size_t *child_idx{nullptr};
-  std::size_t size_{0};
-  std::size_t cap_{0};
-
-public:
-  ~child_idx_vector() { delete[] child_idx; }
-
-  child_idx_vector() = default;
-
-  child_idx_vector(child_idx_vector const &other)
-      : size_{other.size_}, cap_{other.cap_} {
-    if (other.child_idx) {
-      child_idx = new std::size_t[cap_];
-      std::memcpy(child_idx, other.child_idx, size_ * sizeof(std::size_t));
-      return;
-    }
-    std::memcpy(static_child_idx_, other.static_child_idx_,
-                size_ * sizeof(std::size_t));
-  }
-
-  child_idx_vector(child_idx_vector &&other)
-      : child_idx{other.child_idx}, size_{other.size_}, cap_{other.cap_} {
-    std::memcpy(static_child_idx_, other.static_child_idx_, N);
-    other.child_idx = nullptr;
-  }
-
-  bool empty() const { return size_ == 0; }
-
-  std::size_t size() const { return size_; }
-
-  std::size_t *begin() {
-    if (child_idx)
-      return child_idx;
-    return static_child_idx_;
-  }
-
-  std::size_t *end() { return begin() + size_; }
-
-  std::size_t const *begin() const {
-    if (child_idx)
-      return child_idx;
-    return static_child_idx_;
-  }
-
-  std::size_t const *end() const { return begin() + size_; }
-
-  void erase(std::size_t *it) {
-    BOOST_ASSERT(it - begin() >= 0);
-    std::memmove(it - 1, it, end() - it);
-    --size_;
-  }
-
-  void push_back(std::size_t v) {
-    if (size_ == N && !child_idx) {
-      child_idx = new std::size_t[N * 2];
-      cap_ = N * 2;
-      std::memcpy(child_idx, static_child_idx_, N * sizeof(std::size_t));
-    } else if (child_idx && size_ == cap_) {
-      auto *tmp = new std::size_t[cap_ * 2];
-      std::memcpy(tmp, child_idx, cap_ * sizeof(std::size_t));
-      delete[] child_idx;
-      child_idx = tmp;
-      cap_ = cap_ * 2;
-    }
-    begin()[size_++] = v;
-  }
-};
+using child_idx_vector = std::vector<std::size_t>;
 
 // A node in the resource tree
 // Each segment in the resource tree might be
@@ -206,7 +131,7 @@ struct node {
   static constexpr std::size_t npos{std::size_t(-1)};
 
   // literal segment or replacement field
-  detail::segment_template seg{};
+  segment_template seg{};
 
   // A pointer to the resource
   router_base::any_resource const *resource{nullptr};
@@ -219,7 +144,7 @@ struct node {
   std::size_t parent_idx{npos};
 
   // Index of child nodes in the pool
-  detail::child_idx_vector child_idx;
+  child_idx_vector child_idx;
 };
 
 class impl {
