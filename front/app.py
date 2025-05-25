5fd2c9389afe9c245cfc917958ef0d91d62b1459
xin0nix <xin0nix@yahoo.com>
2025-05-25 14:27:44 +0300

    Add basic Game listing funcitonality

--- front/app.py
+++ front/app.py
@@ -2,7 +2,7 @@ import asyncio
 
 from flask import Flask, render_template, redirect, request, url_for, flash
 from game_api_client import Client
-from game_api_client.api.default import create_game
+from game_api_client.api.default import create_game, list_games
 from game_api_client.errors import UnexpectedStatus
 
 app = Flask(__name__)
@@ -32,10 +32,11 @@ def create_game_route():
 
     try:
         game_url = asyncio.run(create_game_async())
-        if game_url and game_url.url.startswith("/games/"):
-            return redirect(game_url.url)
-        else:
-            flash("Unexpected response from server.", "danger")
+        return redirect(url_for("get_games_route"))
+        # if game_url and game_url.url.startswith("/games/"):
+        #     return redirect(game_url.url)
+        # else:
+        #     flash("Unexpected response from server.", "danger")
     except UnexpectedStatus as e:
         flash(f"API Error: {e.status_code} - {e.content}", "danger")
     except ConnectionError as e:
@@ -48,8 +49,26 @@ def create_game_route():
 
 @app.get("/games")
 def get_games_route():
-    # TODO: make list_games request
-    return render_template("games.html")
+    async def list_games_async():
+        client = Client(base_url=f"http://{CORE_HOST}:{CORE_PORT}", verify_ssl=False)
+        async with client as client:
+            return await list_games.asyncio(client=client)
+
+    try:
+        game_list = asyncio.run(list_games_async())
+        if game_list:
+            print(game_list)
+            return render_template("games.html", game_list=game_list)
+        else:
+            flash("Unexpected response from server.", "danger")
+    except UnexpectedStatus as e:
+        flash(f"API Error: {e.status_code} - {e.content}", "danger")
+    except ConnectionError as e:
+        flash(f"Connection failed: {str(e)}", "danger")
+    except Exception as e:
+        flash(f"Unexpected error: {str(e)}", "danger")
+
+    # return redirect(url_for("games"))
 
 
 if __name__ == "__main__":
