8484a676c11ebc1a962aa3616a218906f29bc817
xin0nix <xin0nix@yahoo.com>
2025-05-25 14:46:04 +0300

    Add game status menu

--- front/app.py
+++ front/app.py
@@ -1,9 +1,8 @@
 import asyncio
 
-from flask import Flask, render_template, redirect, request, url_for, flash
+from flask import Flask, render_template, redirect, url_for, flash
 from game_api_client import Client
-from game_api_client.api.default import create_game, list_games
-from game_api_client.errors import UnexpectedStatus
+from game_api_client.api.default import create_game, get_game, list_games
 
 app = Flask(__name__)
 app.secret_key = "####"  # Needed for flashing messages
@@ -19,8 +18,13 @@ def index():
 
 @app.route("/games/<uuid:game_id>/")
 def game_with_uuid(game_id):
-    print(game_id)
-    return render_template("game_with_uuid.html")
+    async def get_game_async():
+        client = Client(base_url=f"http://{CORE_HOST}:{CORE_PORT}", verify_ssl=False)
+        async with client as client:
+            return await get_game.asyncio(client=client, uuid=game_id)
+
+    game_info = asyncio.run(get_game_async())
+    return render_template("game_with_uuid.html", game_info=game_info)
 
 
 @app.post("/games")
@@ -30,20 +34,11 @@ def create_game_route():
         async with client as client:
             return await create_game.asyncio(client=client)
 
-    try:
-        game_url = asyncio.run(create_game_async())
-        return redirect(url_for("get_games_route"))
-        # if game_url and game_url.url.startswith("/games/"):
-        #     return redirect(game_url.url)
-        # else:
-        #     flash("Unexpected response from server.", "danger")
-    except UnexpectedStatus as e:
-        flash(f"API Error: {e.status_code} - {e.content}", "danger")
-    except ConnectionError as e:
-        flash(f"Connection failed: {str(e)}", "danger")
-    except Exception as e:
-        flash(f"Unexpected error: {str(e)}", "danger")
-
+    game_url = asyncio.run(create_game_async())
+    if game_url and game_url.url.startswith("/games/"):
+        return redirect(game_url.url)
+    else:
+        flash("Unexpected response from server.", "danger")
     return redirect(url_for("games"))
 
 
@@ -54,21 +49,12 @@ def get_games_route():
         async with client as client:
             return await list_games.asyncio(client=client)
 
-    try:
-        game_list = asyncio.run(list_games_async())
-        if game_list:
-            print(game_list)
-            return render_template("games.html", game_list=game_list)
-        else:
-            flash("Unexpected response from server.", "danger")
-    except UnexpectedStatus as e:
-        flash(f"API Error: {e.status_code} - {e.content}", "danger")
-    except ConnectionError as e:
-        flash(f"Connection failed: {str(e)}", "danger")
-    except Exception as e:
-        flash(f"Unexpected error: {str(e)}", "danger")
-
-    # return redirect(url_for("games"))
+    game_list = asyncio.run(list_games_async())
+    if game_list:
+        print(game_list)
+        return render_template("games.html", game_list=game_list)
+    else:
+        flash("Unexpected response from server.", "danger")
 
 
 if __name__ == "__main__":
